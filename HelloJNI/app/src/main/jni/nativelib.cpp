50f3e7a2af5c612a03999bf1f4fb3f21eb685727
Do Van Quyen <bbtechlab.vn@gmail.com>
2020-11-15 01:36:58 +0700

    [gnu_make] remove unused

--- HelloJNI/app/src/main/jni/nativelib.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by vqdo on 1/22/2019.
-//
-#include "com_bbtechlab_hellojni_helloStringJNI.h"
-
-JNIEXPORT jstring JNICALL Java_com_bbtechlab_hellojni_helloStringJNI_getStringJNI (JNIEnv *env, jobject obj) {
-    return (*env).NewStringUTF("hello JNI - Bamboo");
-}
-
