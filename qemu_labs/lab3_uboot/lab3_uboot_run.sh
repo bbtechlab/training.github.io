16a734b58d61def04d733b9f405fd848263ffd81
BBTECH Lab <dovanquyen.vn@gmail.com>
2021-03-11 00:59:35 +0700

    [#5] Setup U-BOOT env for loading Kernel/rootfs
    Setup network between QEMU and host - TAP interfaces
    - https://en.wikibooks.org/wiki/QEMU/Networking#TAP/TUN_device

---
+++ qemu_labs/lab3_uboot/lab3_uboot_run.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+sudo /etc/qemu-ifup tap0
+qemu-system-arm -M vexpress-a9 -m 512M -nographic -kernel vexpress/u-boot -net nic -net tap,ifname=tap0,script=no,downscript=no
+sudo /etc/qemu-ifdown tap0
