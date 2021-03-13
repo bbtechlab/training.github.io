b990ba5eaa96775b1d67b0a894a908a1dad1a01e
bamboo <dovanquyen.vn@gmail.com>
2021-03-12 23:34:18 -0800

    [#5] Update lab3_uboot - How U-BOOT load kernel/rootfs, setup network interface between QEMU ARM and Host Environment to verify the booting system through TFTP/NFS

--- qemu_labs/lab3_uboot/build_date.c
+++ qemu_labs/lab3_uboot/build_date.c
@@ -1,2 +1,2 @@
-const char *g_strAppBuildDate = "Thứ sáu, 05 Tháng ba năm 2021 11:58:07 +07";
+const char *g_strAppBuildDate = "Fri Mar 12 21:27:14 PST 2021";
 const char *g_strAppBuildUser = "bamboo@bbtechlab";
